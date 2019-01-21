b7bc175c76de79c1fe84611d2d9554a388986cfe
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-21 18:23:57 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinizer
   
   def piglatinize(word)
     
-    consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+    consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z", "H"]
     vowels = ["a","e","i","o","u"]
     
     if word == word.split(" ")[0]
