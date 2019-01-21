5c0aa444d0e799cd932205b291bafd7cb4bbbfc9
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-21 17:57:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/user_input' do
+   get '/user_input' do
     erb :user_input
    end
    
-   post '/team' do
+   post '/result' do
+     
+     erb :result
+   end
   
 end
